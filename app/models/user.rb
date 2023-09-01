efc7263f7efc075c3fd48628e96cb7aaa3cc963e
onizuka <dark_sao@mail.ru>
2023-09-01 15:26:54 +0300

    update has secrure password and add old password for update user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
+  attr_accessor :old_password
+
+  has_secure_password validations: false 
   validates :name, presence: true, uniqueness: true
+  validate :password_presence
+  
+  validate :correct_old_password, on: :update, if: -> { password.present? }
+  validates :password, confirmation: true, allow_blank: true
+  
+  validate :password_complexity
   after_destroy :ensure_an_admin_remains
-
-  has_secure_password
-
+  
   class Error < StandardError
   end
 
@@ -15,4 +22,21 @@ class User < ApplicationRecord
     end
   end
 
+  def correct_old_password
+    return if BCrypt::Password.new(password_digest_was).is_password?(old_password)
+
+    errors.add :old_password, 'is incorrect'
+  end
+
+  def password_presence
+    errors.add(:password, :blank) if password_digest.blank?
+  end
+
+  def password_complexity
+    return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+
+    msg = 'complexity requirement not met. Length should be 8-70 characters and ' \
+          'include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+    errors.add :password, msg
+  end
 end
